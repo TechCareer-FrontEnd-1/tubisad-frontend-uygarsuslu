cfa5783a5e7fae24e8d393eb231e6f21261b66f9
Uygar Süslü <uygarsuslu1@gmail.com>
2022-06-28 21:00:24 +0300

    template updated

---
+++ templates/template.js
@@ -0,0 +1,21 @@
+let date = () => {
+    // document.getElementById("date1").innerHTML = new Date()
+
+    // $('selector').action()
+    // $('#date1').text(new Date())
+    // $('#date1').html(new Date())
+    $('#date1').html(`Tarih: ${new Date()} `)
+
+    // document.getElementById("date1").style.color = "white"
+    // document.getElementsByClassName("date1")[0].style.backgroundColor = "blue"
+
+    // $("#date1").css("background-color", "blue").css("color", "white")
+
+    const cssObject = {
+        "color": "red",
+        "background-color": "black",
+        "box-shadow": "1px 1px 3px 1px rgb(255,255,255,.8)",
+        "margin-bottom": "3rem"
+    }
+    $("#date1").css(cssObject)
+}
