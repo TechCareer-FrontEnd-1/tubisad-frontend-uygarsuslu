f6cd282c70729c7c6c64365f70b8ae1ca58f6afb
Uygar Süslü <uygarsuslu1@gmail.com>
2022-07-15 22:03:28 +0300

    autocomplete added

--- templates/template.js
+++ templates/template.js
@@ -19,3 +19,34 @@ let date = () => {
     }
     $("#date1").css(cssObject)
 }
+
+/* autocomplete */
+$(function () {
+    var availableTags = [
+        "ActionScript",
+        "AppleScript",
+        "Asp",
+        "BASIC",
+        "C",
+        "C++",
+        "Clojure",
+        "COBOL",
+        "ColdFusion",
+        "Erlang",
+        "Fortran",
+        "Groovy",
+        "Haskell",
+        "Java",
+        "JavaScript",
+        "Lisp",
+        "Perl",
+        "PHP",
+        "Python",
+        "Ruby",
+        "Scala",
+        "Scheme"
+    ];
+    $("#tags").autocomplete({
+        source: availableTags
+    });
+});
